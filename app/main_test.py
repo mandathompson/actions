16d0e8c0d070d292e8a057902758ccea3865a153
Amanda Thompson <athompson@zoll.com>
2023-06-29 18:43:11 -0400

    oops

--- app/main_test.py
+++ app/main_test.py
@@ -5,8 +5,8 @@ import os
 
 class TestMain(unittest.TestCase):
     def test_return_backwards_string(self):
-        random_string = "This is a random string!"
-        random_string_reversed = "!gnirts modnar a si sihT"
+        random_string = "Yo"
+        random_string_reversed = "oY"
         self.assertEqual(random_string_reversed,
                          return_backwards_string(random_string))
 
